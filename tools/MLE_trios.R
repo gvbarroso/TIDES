2db79064acc3c8b32bab477e36f42b6d4b2a0048
gvbarroso <gvbarroso@gmail.com>
2023-01-25 17:07:49 -0600

    improves MLE scripts

--- tools/MLE_trios.R
+++ tools/MLE_trios.R
@@ -6,28 +6,32 @@
 library(stats4)
 library(iterpc)
 
-counts <- read.table("genotype_counts.txt")
+file <- readLines("genotype_counts.txt")
+obs <- cbind.data.frame(t(rbind.data.frame(strsplit(counts[9:15], " "))))[11]
+row.names(obs) <- paste("c", 1:7, sep="")
+colnames(obs) <- "count"
+obs[,1] <- as.numeric(obs[,1])
 
-obs <- c(a, b, c, d, e, f, g)
-obs <- c(100, 82, 43, 71, 34, 36, 24)
-obs <- obs
-
-LogLik <- function(s, h, a, b, c, d, e, f, g) {
+LogLik <- function(s, h, c1, c2, c3, c4, c5, c6, c7) {
   
   w1 <- (2 + h*s ) / 2
   w2 <- (4 + 2*h*s + s) / 4
   w3 <- (2 + h*s + s) / 2
   
-  #mult <- multichoose(c(a, b, c, d, e, f, g), bigz=TRUE) 
-  unifProb <- ((1/(2*w1))^a) * (((1 + h*s)/(2*w1))^b) * ((1/(4*w2))^c) * (((1 + h*s)/(2*w2))^d) * (((1 + s)/(4*w2))^e) * (((1 + h*s)/(2*w3))^f) * (((1 + s)/(2*w3))^g)
-
-  cat(paste(unifProb, "\n"))
+  mult <- multichoose(c(c1, c2, c3, c4, c5, c6, c7), bigz=TRUE) 
   
-  #return(-log(mult * unifProb))
-  return(-log(unifProb))
+  # the following runs into underflow issues:
+  #orderedProb <- ((1/(2*w1))^c1) * (((1 + h*s)/(2*w1))^c2) * ((1/(4*w2))^c3) * (((1 + h*s)/(2*w2))^c4) * (((1 + s)/(4*w2))^c5) * (((1 + h*s)/(2*w3))^c6) * (((1 + s)/(2*w3))^c7)
+  #return(-log(mult * orderedProb)) # R cannot multiply a ridiculously large number by a ridiculously small one
+  
+  # instead we work directly in log space:
+  logOrderedProb <- c1*log(1/(2*w1)) + c2*log((1 + h*s)/(2*w1)) + c3*log(1/(4*w2)) + c4*log((1 + h*s)/(2*w2)) + c5*log((1 + s)/(4*w2)) + c6*log((1 + h*s)/(2*w3)) + c7*log((1 + s)/(2*w3))
+  
+  
+  return(-(log(mult) + logOrderedProb)) 
 }
 
 fit <- mle(LogLik, method="L-BFGS-B", start=list(s=-0.005, h=0.25), lower=list(s=-0.1, h=0), upper=(list(s=0, h=1)), 
-           fixed=list(a=obs[1], b=obs[2], c=obs[3], d=obs[4], e=obs[5], f=obs[6], g=obs[7]))
+           fixed=list(c1=obs[1,], c2=obs[2,], c3=obs[3,], c4=obs[4,], c5=obs[5,], c6=obs[6,], c7=obs[7,]))
 
 summary(fit)
